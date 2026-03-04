fd425937714a51563870ed3856e54f4c9cd56934
maisaperrut <maisaperrutvasco@gmail.com>
2026-03-04 12:50:48 -0300

    DOMfinance main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 function App() {
   return (
     <div style={{padding: "40px", fontFamily: "Arial"}}>
-      <h1>DevFinance Online 🚀</h1>
+      <h1>DOMfinance Online 🚀</h1>
       <p>Seu sistema está funcionando!</p>
     </div>
   );
